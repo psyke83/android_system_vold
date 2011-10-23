441e6440a7be637e85ce343552eed56fa32d5b2b
FUKAUMI Naoki <naobsd@gmail.com>
2011-10-24 03:01:41 +0900

    fix buffer overflow while reading /proc/cmdline
    char line[255] was used to store each line in vold.fstab. but, by AutoVolume changes, it is also (ab)used to store each arg in /proc/cmdline.
    length of /proc/cmdline is architecture dependent. it should be 1024 on arm. so each arg can be longer than 255.
    
    this change fixes vold crash (stack corruption detected: aborted) with longer arg.
    
    this issue is found by ^BatVanko^ on forum.xda-developers.com. thank you!
    
    Change-Id: I7a4a9dd00a9a0b291429df9a85fbe097fd22c1ba

--- main.cpp
+++ main.cpp
@@ -162,11 +162,11 @@ static int parse_mount_flags(char *mount_flags)
 static int process_config(VolumeManager *vm) {
     FILE *fp;
     int n = 0;
-    char line[255];
+    char line[1024];
     Volume *vol = 0;
 
     if ((fp = fopen("/proc/cmdline", "r"))) {
-        while (fscanf(fp, "%s", line) > 0) {
+        while (fscanf(fp, "%1023s", line) > 0) {
             if (!strncmp(line, "SDCARD=", 7)) {
                 const char *sdcard = line + 7;
                 if (*sdcard) {
